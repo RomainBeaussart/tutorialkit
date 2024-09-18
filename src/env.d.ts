0521f141367f63d595e78bc001a4ec2725459165
Romain <r.beaussart62@gmail.com>
2024-09-18 10:18:21 +0200

    Remove unused User type definition and instance from env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,20 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@tutorialkit/astro/types" />
 /// <reference types="astro/client" />
-
-
-type User = {
-    name: string;
-    email: string;
-    password: string;
-    id: string;
-}
-
-const user: User = {
-    name: 'John Doe',
-    email: 'john.doe@exemple.com',
-    password: 'password',
-    id: '123456789'
-}
-
-user.
